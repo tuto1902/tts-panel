8ca62ee2297ef14541172fb235045f122087b764
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2025-02-03 21:02:56 -0600

    Lint fix

--- routes/console.php
+++ routes/console.php
@@ -14,7 +14,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Artisan::command('twitch:event', function(){
+Artisan::command('twitch:event', function () {
     $quote = Inspiring::quotes()->random();
 
     broadcast(
@@ -22,33 +22,33 @@ Artisan::command('twitch:event', function(){
     );
 });
 
-Artisan::command('twitch:status', function (){
+Artisan::command('twitch:status', function () {
     $response = spin(
         message: 'Fetching new access token...',
-        callback: fn () => Http::post("https://id.twitch.tv/oauth2/token", [
-            "client_id" => config("services.twitch.client_id"),
-            "client_secret" => config("services.twitch.client_secret"),
-            "grant_type" => "client_credentials"
+        callback: fn () => Http::post('https://id.twitch.tv/oauth2/token', [
+            'client_id' => config('services.twitch.client_id'),
+            'client_secret' => config('services.twitch.client_secret'),
+            'grant_type' => 'client_credentials',
         ])
     );
 
-    $accessToken = $response->json("access_token");
+    $accessToken = $response->json('access_token');
 
     $response = spin(
         message: 'Fetching twitch event subscription status',
         callback: fn () => Http::withToken($accessToken)
             ->withHeaders([
-                "Client-Id" => config("services.twitch.client_id"),
-                "Content-Type" => "application/json"
+                'Client-Id' => config('services.twitch.client_id'),
+                'Content-Type' => 'application/json',
             ])
-            ->get("https://api.twitch.tv/helix/eventsub/subscriptions")
-        );
+            ->get('https://api.twitch.tv/helix/eventsub/subscriptions')
+    );
 
     $response = $response->json();
     $rows = collect($response['data'])->map(fn ($row) => [
-            'id' => $row['id'],
-            'status' => $row['status'],
-            'webhook' => $row['transport']['callback']
+        'id' => $row['id'],
+        'status' => $row['status'],
+        'webhook' => $row['transport']['callback'],
     ]);
     table(
         headers: ['ID', 'Status', 'Webhook URL'],
