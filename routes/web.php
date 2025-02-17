2b903ee8bd6f21f037fe8d3e2fe734a8e848b17b
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2025-02-17 14:42:40 -0600

    Google TTS test

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,15 @@ use App\Http\Controllers\TwitchController;
 use App\Http\Middleware\TwitchValidationMiddleware;
 use App\Livewire\Pages\ShowOverlay;
 use App\Livewire\Pages\ShowTwitchEvents;
+use Google\Cloud\TextToSpeech\V1\AudioConfig;
+use Google\Cloud\TextToSpeech\V1\AudioEncoding;
+use Google\Cloud\TextToSpeech\V1\Client\TextToSpeechClient;
+use Google\Cloud\TextToSpeech\V1\SsmlVoiceGender;
+use Google\Cloud\TextToSpeech\V1\SynthesisInput;
+use Google\Cloud\TextToSpeech\V1\SynthesizeSpeechRequest;
+use Google\Cloud\TextToSpeech\V1\VoiceSelectionParams;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 Route::get('/', function () {
     return view('welcome');
@@ -26,6 +34,24 @@ Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified']
     Route::get('/events', ShowTwitchEvents::class)->name('events.index');
 
     Route::get('/clip', [TwitchController::class, 'clip']);
+
+    Route::get('/google', function() {
+        $textToSpeechClient = new TextToSpeechClient();
+        $input = new SynthesisInput();
+        $input->setText('This is my first test using Google Text To Speech service. Hooray!');
+        $voice = new VoiceSelectionParams();
+        $voice->setLanguageCode('en-GB');
+        $voice->setName('en-GB-Studio-B');
+        $voice->setSsmlGender(SsmlVoiceGender::MALE);
+        $audioConfig = new AudioConfig();
+        $audioConfig->setAudioEncoding(AudioEncoding::MP3);
+        $request = new SynthesizeSpeechRequest();
+        $request->setInput($input);
+        $request->setVoice($voice);
+        $request->setAudioConfig($audioConfig);
+        $response = $textToSpeechClient->synthesizeSpeech($request);
+        Storage::disk('public')->put('google-test.mp3', $response->getAudioContent());
+    });
 });
 
 Route::get('/overlay', ShowOverlay::class);
